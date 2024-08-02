7f487e29ee822a23473fa9d9dfc432e0a8a53f36
NikitaBaranov43 <fast.flou@gmail.com>
2024-08-02 20:25:09 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second = int(input(''))
 third = int(input(''))
 if first == second and second == third and first == third:
     print(3)
-elif first == second or first == third:
+elif first == second or first == third or second == third:
     print(2)
-elif first != second and second != third:
+else:
     print(0)
